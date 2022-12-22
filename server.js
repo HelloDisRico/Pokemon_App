993e188238166b0b1feb2500ce55a1b4bf739483
Kevin Rico <krico315@gmail.com>
2022-12-22 01:18:20 -0700

    server is running

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
+const pokemon = require('./models/pokemon.js')
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine());
 
 app.listen(port, function () {
     console.log("Server is runnin' on port 3000")
@@ -9,3 +13,7 @@ app.listen(port, function () {
 app.get ("/" , (req,res) => {
     res.send("Welcome to the Pokemon App!")
 })
+app.get('/pokemon/', (req,res) => {
+    res.render('Index',{pokemon:pokemon});
+});
+
