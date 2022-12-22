285f80e69d1a53b4bcd8b191bc0692a8f001d537
Kevin Rico <krico315@gmail.com>
2022-12-22 01:26:01 -0700

    connect to server, see json in browser

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.listen(port, function () {
 app.get ("/" , (req,res) => {
     res.send("Welcome to the Pokemon App!")
 })
-app.get('/pokemon/', (req,res) => {
-    res.render('Index',{pokemon:pokemon});
+app.get('/pokemon', (req, res) => {
+  res.send(pokemon);
 });
+
 
