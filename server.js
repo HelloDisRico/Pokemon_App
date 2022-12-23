7540c49a614de8ebc8bd676732609e3a2249b96a
Kevin Rico <krico315@gmail.com>
2022-12-22 23:44:24 -0700

    goose

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const express = require('exoress')
+require('dotenv').config();
+const express = require('express')
 const app = express();
+const mongoose = require('mongoose')
 
 const pokemon = require('./models/pokemon')
+
+const MONGO_URI = process.hasUncaughtExceptionCaptureCallback.MONGO_URI;
+const config = {
+    useNewUrlParser: true,
+    useUnifiedToplogy: true,
+};
+const methodOverride = require('method-override');
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedToplogy: true,
+})
 
 app.set('view engine', 'jsx');
 app.engine('jsx',require('express-react-views').createEngine());
